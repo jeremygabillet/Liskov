9a0270b84f4891d9a796a6af544f04b1b11385c7
jeremygabillet <jeremy.gabillet@gmail.com>
2021-12-06 11:20:42 +0100

    Finishing quest

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Liskov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Polygon square = new Square(3);
+            Polygon rectangle = new Rectangle(2, 3);
+            System.Console.WriteLine(square.Area);
+            System.Console.WriteLine(rectangle.Area);
+        }
+    }
+
+    abstract class Polygon
+    {
+        public double Height { get; }
+        public double Width { get; }
+        public abstract double Area { get; }
+        public Polygon(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+        
+    }
+    class Square : Polygon
+    {
+        public override double Area 
+        { 
+            get 
+            {
+                return Height * Width;
+            }
+        }
+        public Square(double side) : base(side, side) 
+        {
+        }
+    }
+    class Rectangle : Polygon
+    {
+        public override double Area 
+        {
+            get
+            {
+                return Height * Width;
+            }
+        }
+        public Rectangle(double height, double width) : base(height, width)
+        {
+        }
+    }
+}
